c2b8102e1f974bf4d2ce61cbcae3c5dd5a17c10c
Isaackiux <116937683+Isaackiux@users.noreply.github.com>
2026-04-25 16:15:54 -0700

    Delete config directory

--- config/backend_config_steam.gd
+++
@@ -1,8 +0,0 @@
-class_name SteamBackendConfig
-extends Resource
-## Steam backend specific configuration.
-
-## Maximum time (seconds) for the client Steam connection to complete after joining a lobby.
-@export var connection_timeout: float = 10.0
-## Steam lobby type: 0=Private, 1=FriendsOnly, 2=Public, 3=Invisible.
-@export var lobby_type: int = 2
